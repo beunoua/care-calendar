a43e3440b28d707040aa33e21aafdfd5e331cc5a
Benoist LAURENT <benoist.laurent@gmail.com>
2021-08-25 22:56:44 +0200

    remove useless cli unit test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,11 +1,2 @@
 """Tests for `care_calendar` command-line interface."""
 
-import unittest
-
-from care_calendar import cli
-
-
-class TestFoo(unittest.TestCase):
-    def test_foo(self):
-        cli.main()
-        # self.fail()
