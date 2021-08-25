beunoua/care-calendar

commit 97f11c371e9a3918ce6f72531a11cb6461603e86

refactoring: date_range_to_list -> date_range_from_str

care_calendar/status.py
@@ -63,11 +63,14 @@ def str_to_date(date_string: str, year: int = None) -> datetime.date:
         raise ValueError(f"day is out of range: {year}/{month}/{day}") from exc
 
 
-def date_range_to_list(
-    daterange: tuple[str, str], year: int = None
-) -> List[datetime.date]:
+def date_range_from_str(daterange: str, year: int = None) -> List[datetime.date]:
     """Returns a list of dates from a string representing a date range."""
     start, end = [str_to_date(s, year) for s in daterange.split("-")]
+    return date_range(start, end)
+
+
+def date_range(start: datetime.date, end: datetime.date) -> List[datetime.date]:
+    """Returns a list of dates between two dates."""
     return [start + datetime.timedelta(days=i) for i in range((end - start).days + 1)]
 
 
@@ -85,7 +88,7 @@ def read_status_yaml(path: str, year: int = None):
                     raise ValueError(f"category: {category}: empty date string")
                 # date is a range.
                 if "-" in datestr:
-                    dates = date_range_to_list(datestr, year)
+                    dates = date_range(datestr, year_from_str)
                     status.add_range(dates)
                     status.date_list.extend(dates)
                 else:

tests/test_status.py
@@ -7,7 +7,7 @@ import unittest
 
 from care_calendar.status import (
     str_to_date,
-    date_range_to_list,
+    date_range_from_str,
     read_status_yaml,
     Status,
 )
@@ -79,17 +79,17 @@ class TestStrToDate(unittest.TestCase):
             str_to_date(date_str, 2021)
 
 
-class TestDateRangeToList(unittest.TestCase):
-    """Tests for care_calendar.status.date_range_to_list."""
+class TestDateRangeFromStr(unittest.TestCase):
+    """Tests for care_calendar.status.date_range_from_str."""
 
     def test_returns_a_list_of_dates(self):
-        dates = date_range_to_list("13/02 - 15/02", 1912)
+        dates = date_range_from_str("13/02 - 15/02", 1912)
         self.assertIsInstance(dates, list)
         for date in dates:
             self.assertIsInstance(date, datetime.date)
 
     def test_returns_correct_dates(self):
-        dates = date_range_to_list("13/02 - 15/02", 1912)
+        dates = date_range_from_str("13/02 - 15/02", 1912)
         self.assertEqual(len(dates), 3)
         for i, day in enumerate(range(13, 16)):
             self.assertEqual(dates[i].day, day)
@@ -97,7 +97,7 @@ class TestDateRangeToList(unittest.TestCase):
             self.assertEqual(dates[i].year, 1912)
 
     def test_returns_correct_dates_no_year(self):
-        dates = date_range_to_list("13/02 - 15/02", 1912)
+        dates = date_range_from_str("13/02 - 15/02", 1912)
         self.assertEqual(len(dates), 3)
         for i, day in enumerate(range(13, 16)):
             self.assertEqual(dates[i].day, day)
@@ -105,7 +105,7 @@ class TestDateRangeToList(unittest.TestCase):
             self.assertEqual(dates[i].year, 1912)
 
     def test_accross_years(self):
-        dates = date_range_to_list("28/12/20 - 06/01/21")
+        dates = date_range_from_str("28/12/20 - 06/01/21")
         self.assertEqual(len(dates), 10)
         for i, day in enumerate(range(28, 32)):
             self.assertEqual(dates[i].day, day)

commit 8bd0bb2e1e75c51b2fc4d5bc8c56657d2e67520a

bufix: fix bug introduced in 97f11c3

care_calendar/status.py
@@ -88,7 +88,7 @@ def read_status_yaml(path: str, year: int = None):
                     raise ValueError(f"category: {category}: empty date string")
                 # date is a range.
                 if "-" in datestr:
-                    dates = date_range(datestr, year_from_str)
+                    dates = date_range(datestr, year)
                     status.add_range(dates)
                     status.date_list.extend(dates)
                 else: